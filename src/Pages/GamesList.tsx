45d5cf0b0d0315ea55fcfb69d2c431e5d4aa5223
Serhii Voitovych <vsv0218@gmail.com>
2024-05-09 12:54:00 +0300

    dd

--- src/Pages/GamesList.tsx
+++ src/Pages/GamesList.tsx
@@ -1,37 +1,36 @@
+const games = [{
+    player1: 'Player1',
+    wins: 2
+},
+{
+    player1: 'Player2',
+    wins: 3
+},
+{
+    player1: 'Player3',
+    wins: 7
+},
+{
+    player1: 'Player4',
+    wins: 1
+},
+{
+    player1: 'Player5',
+    wins: 5
+}]
+
 export const GamesList = () => {
     return (<>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div>
-                Player1
+        {games.map(s => (
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div>
+                    <input type='checkbox' />{s.player1}
+                </div>
+                <div>
+                    Wins: {s.wins}
+                </div>
             </div>
-            <div>
-                Player2
-            </div>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div>
-                Player1
-            </div>
-            <div>
-                Player2
-            </div>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div>
-                Player1
-            </div>
-            <div>
-                Player2
-            </div>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div>
-                Player1
-            </div>
-            <div>
-                Player2
-            </div>
-        </div>
+        ))}
     </>
     )
 }
