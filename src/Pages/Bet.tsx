09b4033e445fdd752bcc533778968881c269ed57
Serhii Voitovych <vsv0218@gmail.com>
2024-05-09 13:37:48 +0300

    dd

--- src/Pages/Bet.tsx
+++ src/Pages/Bet.tsx
@@ -1,23 +1,31 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { IPlayer, useTelegram } from "../App"
 
-export const Bet = ({player}: {player: IPlayer}) => {
+export const Bet = ({ player }: { player: IPlayer }) => {
     const [amount, setAmount] = useState<number>(0);
-    const {webApp} = useTelegram();
-    useEffect(()=>{
-        if (!amount){
+    const { webApp } = useTelegram();
+
+    const mainButtonClickedHandler = useCallback(() => {
+        webApp?.sendData(`/bet ${player.id} ${amount}`);
+    }, [webApp, amount, player.id])
+
+    useEffect(() => {
+        if (!amount) {
             webApp?.MainButton.disable();
         } else {
             webApp?.MainButton.enable();
             webApp?.MainButton.setText(`Bet ${amount} on player ${player.player1}`);
         }
-    }, [amount, player.player1, webApp?.MainButton])
 
+        webApp?.onEvent('mainButtonClicked', mainButtonClickedHandler);
+
+        return webApp?.offEvent('mainButtonClicked', mainButtonClickedHandler);
+    }, [amount, player.player1, webApp?.MainButton, mainButtonClickedHandler, webApp])
     return (
         <>
             <form>
                 <div>Bet on player: {player.player1}</div>
-                <input type="number" title="Amount" value={amount} onChange={(e)=>setAmount(+e.target.value)}/> 
+                <input type="number" title="Amount" value={amount} onChange={(e) => setAmount(+e.target.value)} />
             </form>
         </>
     )
